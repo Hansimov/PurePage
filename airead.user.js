1e9f2c7d2e62f5d134183fec10fba88c90c6aed6
Hansimov <591172499@qq.com>
2024-03-16 22:55:31 +0800

    :gem: [Feature] Filter reading_elements with removable patterns

--- airead.user.js
+++ airead.user.js
@@ -10,21 +10,23 @@
 // @require      file:///E:/_codes/airead/airead.user.js
 // ==/UserScript==
 
+// Informative Elements
+
 const HEADER_TAGS = ["h1", "h2", "h3", "h4", "h5", "h6"];
-const FIGURE_TAGS = ["figure"];
 const TABLE_TAGS = ["table"];
 const PRE_TAGS = ["pre"];
-const MATH_TAGS = ["math"];
 const BLOCKQUOTE_TAGS = ["blockquote"];
 const IMG_TAGS = ["img"];
 const CAPTION_TAGS = ["figcaption"];
 
 const GROUP_TAGS = ["div", "section"];
 const LIST_TAGS = ["ul", "ol"];
+const FIGURE_TAGS = ["figure"];
 
 const P_TAGS = ["p"];
 const LI_TAGS = ["li"];
 
+const MATH_TAGS = ["math"];
 const CODE_TAGS = ["code"];
 const LINK_TAGS = ["a"];
 
@@ -39,11 +41,41 @@ const ATOM_TAGS = [].concat(
 const PARA_TAGS = [].concat(GROUP_TAGS, LIST_TAGS, P_TAGS, LI_TAGS);
 
 const CUSTOM_CSS = `
-.airead-atomized {
+.airead-info-element {
     border: 1px solid red;
-    background-color: #ffcccc;
+    // background-color: #ffcccc;
 }
 `;
+
+// Removable Elements classes and ids
+
+const COMMON_REMOVABLE_CLASSES = [
+    "(?<!has)sidebar",
+    "footer",
+    "related",
+    "comment",
+    "topbar",
+    "offcanvas",
+    "navbar",
+];
+const WIKIPEDIA_REMOVABLE_CLASSES = [
+    "(mw-)((jump-link)|(editsection))",
+    "language-list",
+    "p-lang-btn",
+    "(vector-)((header)|(column)|(sticky-pinned)|(dropdown-content)|(page-toolbar)|(body-before-content)|(settings))",
+    "navbox",
+    "catlinks",
+    "side-box",
+    "contentSub",
+    "siteNotice",
+];
+
+const REMOVABLE_CLASSES = [].concat(
+    COMMON_REMOVABLE_CLASSES,
+    WIKIPEDIA_REMOVABLE_CLASSES
+);
+
+// Helper Functions
 
 function get_tag(element) {
     return element.tagName.toLowerCase();
@@ -67,6 +99,19 @@ function is_elements_has_tag(elements, tags) {
     return elements.some((element) => tags.includes(get_tag(element)));
 }
 
+function is_class_id_match_pattern(element, pattern_str) {
+    let pattern = new RegExp(pattern_str, "i");
+    let parents = get_parents(element);
+    let is_match =
+        pattern.test(element.className) ||
+        pattern.test(element.id) ||
+        parents.some((parent) => pattern.test(parent.className)) ||
+        parents.some((parent) => pattern.test(parent.id));
+    return is_match;
+}
+
+// Main Classes
+
 class ReadableElementsSelector {
     constructor() {}
     is_atomized(element) {
@@ -81,29 +126,46 @@ class ReadableElementsSelector {
             const is_parent_has_atom = is_elements_has_tag(parents, ATOM_TAGS);
             const is_descent_has_para_or_atom = is_elements_has_tag(
                 descents,
-                PARA_TAGS.concat(ATOM_TAGS)
+                [].concat(PARA_TAGS, ATOM_TAGS)
             );
             return !(is_parent_has_atom || is_descent_has_para_or_atom);
         }
         return false;
     }
     select_atomized_elements() {
-        var elements = get_descents(document.body);
-        var atomized_elements = [];
-        for (var i = 0; i < elements.length; i++) {
+        let elements = get_descents(document.body);
+        let atomized_elements = [];
+        for (let i = 0; i < elements.length; i++) {
             if (this.is_atomized(elements[i])) {
                 atomized_elements.push(elements[i]);
             }
         }
         return atomized_elements;
     }
-    add_style_to_atomized_elements() {
-        var atomized_elements = this.select_atomized_elements();
-        for (var i = 0; i < atomized_elements.length; i++) {
-            atomized_elements[i].classList.add("airead-atomized");
+
+    filter_info_elements(elements) {
+        let info_elements = elements;
+        // use regex to filter elements, if class or id of element or its parent match any pattern in REMOVABLE_CLASSES, remove it
+        for (let i = 0; i < REMOVABLE_CLASSES.length; i++) {
+            info_elements = info_elements.filter(
+                (element) =>
+                    !is_class_id_match_pattern(element, REMOVABLE_CLASSES[i])
+            );
+        }
+        return info_elements;
+    }
+
+    add_style_to_reading_elements() {
+        let reading_elements = this.select_atomized_elements();
+        reading_elements = this.filter_info_elements(reading_elements);
+        console.log("Reading elements count:", reading_elements.length);
+        for (let i = 0; i < reading_elements.length; i++) {
+            reading_elements[i].classList.add("airead-info-element");
         }
     }
 }
+
+// Main Function
 
 (function () {
     "use strict";
@@ -114,5 +176,5 @@ class ReadableElementsSelector {
     document.head.appendChild(style_element);
 
     const selector = new ReadableElementsSelector();
-    selector.add_style_to_atomized_elements();
+    selector.add_style_to_reading_elements();
 })();
