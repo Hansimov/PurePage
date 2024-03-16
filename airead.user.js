a015e20897774177cb3d8a1ec7975e01d3906164
Hansimov <591172499@qq.com>
2024-03-16 17:06:38 +0800

    :gem: [Feature] New rules for atomized elements, and upgrade to v0.1

--- airead.user.js
+++ airead.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AIRead
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.1
 // @description  An AI-assisted reading script in browsers.
 // @author       Hansimov
 // @match        http://127.0.0.1:17777/*.html
@@ -11,48 +11,78 @@
 // ==/UserScript==
 
 var HEADER_TAGS = ["h1", "h2", "h3", "h4", "h5", "h6"];
-
-var PARA_TAGS = ["p"];
+var FIGURE_TAGS = ["figure"];
 var TABLE_TAGS = ["table"];
-var LI_TAGS = ["li"];
 var CODE_TAGS = ["pre", "code"];
-var Math_TAGS = ["math"];
+var MATH_TAGS = ["math"];
+var BLOCKQUOTE_TAGS = ["blockquote"];
+// var CAPTION_TAGS = ["figcaption"];
 
-var GROUP_TAGS = ["p", "div", "section"];
-var LINK_TAGS = ["a", "img"];
+var P_TAGS = ["p"];
+var LI_TAGS = ["li"];
+var LINK_TAGS = ["a"];
+
+var GROUP_TAGS = ["div", "section"];
 var LIST_TAGS = ["ul", "ol"];
 
 var ATOM_TAGS = HEADER_TAGS.concat(
-    PARA_TAGS,
-    LI_TAGS,
+    FIGURE_TAGS,
     TABLE_TAGS,
     CODE_TAGS,
-    Math_TAGS
+    MATH_TAGS,
+    BLOCKQUOTE_TAGS
+    // CAPTION_TAGS
 );
+var PARA_TAGS = [].concat(GROUP_TAGS, LIST_TAGS, P_TAGS, LI_TAGS);
 
 console.log("ATOM_TAGS:", ATOM_TAGS);
 
 function get_tag(element) {
     return element.tagName.toLowerCase();
 }
+function get_descents(element) {
+    return element.querySelectorAll("*");
+}
+function get_parents(element) {
+    var parents = [];
+    var parent = element.parentElement;
+    while (parent) {
+        parents.push(parent);
+        parent = parent.parentElement;
+    }
+    return parents;
+}
 
 class ReadableElementsSelector {
     constructor() {}
     is_atomized(element) {
-        // Only atomized if tag is in ATOM_TAGS, and no child is in ATOM_TAGS
-        for (var i = 0; i < element.children.length; i++) {
-            if (ATOM_TAGS.includes(get_tag(element.children[i]))) {
-                return false;
-            }
-        }
-        if (ATOM_TAGS.includes(get_tag(element))) {
+        // If tag is in ATOM_TAGS, return true
+        var tag = get_tag(element);
+        if (ATOM_TAGS.includes(tag)) {
             return true;
-        } else {
-            return false;
         }
+
+        // If tag is in PARA_TAGS, and no parent in ATOM_TAGS, and no descent is in PARA_TAGS, return true
+        var decadents = get_descents(element);
+        var parents = get_parents(element);
+
+        if (PARA_TAGS.includes(tag)) {
+            for (var i = 0; i < decadents.length; i++) {
+                if (PARA_TAGS.includes(get_tag(decadents[i]))) {
+                    return false;
+                }
+            }
+            for (var i = 0; i < parents.length; i++) {
+                if (ATOM_TAGS.includes(get_tag(parents[i]))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
     }
     select_atomized_elements() {
-        var elements = document.getElementsByTagName("*");
+        var elements = get_descents(document.body);
         var atomized_elements = [];
         for (var i = 0; i < elements.length; i++) {
             if (this.is_atomized(elements[i])) {
